bf94ce7a57e739b258fb99c7b4cdb3c39a5b44d8
BondarenkoMM <masya.klas3000@gmail.com>
2026-04-30 15:01:29 +0200

    test commit

---
+++ src/main/java/com/wts/WeatherTemperatureServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTemperatureServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherTemperatureServiceApplication.class, args);
+    }
+
+}
